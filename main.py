66a06f8781fcecbfbd7d3b5cea23dff2a4f0764b
Mathias K. E. Sørensen <warlour@eksworld.dk>
2024-01-03 10:53:21 +0100

    Deleted dataset, it will be downloaded as temporary files

--- main.py
+++ main.py
@@ -16,5 +16,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 all_transforms = transforms.Compose([transforms.Resize((32, 32)),
                                      transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                                                          std=[0.2023, 0.1994, 0.2010])])
+                                     transforms.Normalize(mean=[0.5],
+                                                          std=[0.5])
+                                    ])
+train_dataset = torchvision.datasets.FER2013(root = './data',
+                                             train = True,
+                                             tranform = all_transforms,
+                                             download = True)
