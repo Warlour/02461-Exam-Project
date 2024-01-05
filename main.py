bceb1be166b53de559e69322d573ebbbb120c82e
Mathias K. E. Sørensen <warlour@eksworld.dk>
2024-01-05 16:13:27 +0100

    Test 4

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ if not args.disable_csv:
     convlayers = 4
     pools = 2
     user = "Stationær"
-    note = "Test 3"
+    note = "Test 4"
 
     new_data = {
         "Date & Time":          [], 
