ca17ea0f252155daf0733c12b8bbb679c810b553
Mathias K. E. Sørensen <68961022+Warlour@users.noreply.github.com>
2024-01-07 15:30:21 +0100

    Update model classes with super() call

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class EmotionRecognizerV1(nn.Module):
         self.MEANPOOLS = 0
         self.DROPOUTS = 0
 
-        super(EmotionRecognizerV1, self).__init__()
+        super(self.__class__, self).__init__()
         # in_channels: color channels, black and white is 1, rgb is 3
         self.conv_layer1 = nn.Conv2d(in_channels=3 if rgb else 1, out_channels=32, kernel_size=3)
         self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
@@ -49,7 +49,7 @@ class SimpleEmotionRecognizer(nn.Module):
         self.MEANPOOLS = 0
         self.DROPOUTS = 1
 
-        super(SimpleEmotionRecognizer, self).__init__()
+        super(self.__class__, self).__init__()
         self.conv_layer1 = nn.Conv2d(in_channels=3 if rgb else 1, out_channels=16, kernel_size=3, padding=1)
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
@@ -79,7 +79,7 @@ class EmotionRecognizerV2(nn.Module):
         self.MEANPOOLS = 0
         self.DROPOUTS = 3
 
-        super(EmotionRecognizerV2, self).__init__()
+        super(self.__class__, self).__init__()
         self.conv_layer1 = nn.Conv2d(in_channels=3 if rgb else 1, out_channels=32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)  # Batch normalization
         self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
@@ -105,6 +105,48 @@ class EmotionRecognizerV2(nn.Module):
 
         x = F.relu(self.bn3(self.conv_layer3(x)))
         x = self.max_pool2(F.relu(self.bn4(self.conv_layer4(x))))
+        x = self.dropout2(x)
+
+        x = x.view(x.size(0), -1)  # Flatten layer
+        x = F.relu(self.fc1(x))
+        x = self.dropout3(x)
+        x = self.fc2(x)
+        return x
+
+class EmotionRecognizerV3(nn.Module):
+    def __init__(self, num_classes, rgb=False):
+        self.FCS = 2
+        self.CONVS = 4
+        self.MAXPOOLS = 0
+        self.MEANPOOLS = 2
+        self.DROPOUTS = 3
+
+        super(self.__class__, self).__init__()
+        self.conv_layer1 = nn.Conv2d(in_channels=3 if rgb else 1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)  # Batch normalization
+        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)  # Batch normalization
+        self.avg_pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.dropout1 = nn.Dropout(0.25)  # Dropout
+
+        self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)  # Batch normalization
+        self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)  # Batch normalization
+        self.avg_pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.dropout2 = nn.Dropout(0.25)  # Dropout
+
+        self.fc1 = nn.Linear(4096, 128)
+        self.dropout3 = nn.Dropout(0.5)  # Dropout
+        self.fc2 = nn.Linear(128, num_classes)
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv_layer1(x)))
+        x = self.avg_pool1(F.relu(self.bn2(self.conv_layer2(x))))
+        x = self.dropout1(x)
+
+        x = F.relu(self.bn3(self.conv_layer3(x)))
+        x = self.avg_pool2(F.relu(self.bn4(self.conv_layer4(x))))
         x = self.dropout2(x)
 
         x = x.view(x.size(0), -1)  # Flatten layer
